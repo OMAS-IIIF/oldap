7cd5d55f1b5dfc84a5acb636afc9ad04823d1912
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2025-01-27 01:10:54 +0100

    more progress with user management

--- src/lib/components/UsersList.svelte
+++ src/lib/components/UsersList.svelte
@@ -21,11 +21,13 @@
 
 	let { project = $bindable() } = $props();
 
-	let activeToggleModel = $state(false)
+	let toggleConformModal = $state(false)
+	let toggleChecked: {[key: string]: boolean} = $state({});
+	let userid: string = $state('');
 
-	const authinfo_json = sessionStorage.getItem('authinfo')
+	const authinfo_json = sessionStorage.getItem('authinfo');
 
-	let users: OldapUser[] = $state([]);
+	let users: {[key: string]: OldapUser} = $state({});
 
 	let authinfo: AuthInfo;
 	if (authinfo_json) {
@@ -53,6 +55,7 @@
 			//
 			apiClient.getAdminusersearch(config_usersearch)
 				.then((user_iris) => {
+					console.log("========>", user_iris)
 					user_iris.forEach((x) => {
 						const config_userdata = {
 							queries: { iri: x },
@@ -67,7 +70,8 @@
 						apiClient.getAdminuserget (config_userdata)
 							.then((user_json) => {
 								let user = OldapUser.fromOldapJson(user_json);
-								users.push(user);
+								users[user.userId.toString()] = user;
+								toggleChecked[user.userId.toString()] = user?.isActive ?? false;
 								console.log(user);
 							})
 							.catch((err) => {
@@ -83,15 +87,31 @@
 
 	const toggle_active = (e: Event, userId: string) => {
 		e.preventDefault();
-		activeToggleModel = true;
-		console.log("=====>", e);
+		userid = userId;
+		toggleConformModal = true;
 	}
 
 	const handleCancel = () => {
-		alert("Clicked cancel");
 	};
-	const handleDelete = () => {
-		alert("Clicked delete");
+
+	const handleYes = () => {
+		toggleChecked[userid] = !toggleChecked[userid];
+		const data = {isActive: toggleChecked[userid]}
+		const config_change_isActive = {
+			headers: {
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + authinfo.token,
+			},
+			params: { userId: userid },
+		}
+		apiClient.postAdminuserUserId(data, config_change_isActive)
+			.then((result) => {
+				console.log(result);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		alert("CHANGE ACTIVE STATE TO " + toggleChecked[userid])
 	};
 
 </script>
@@ -106,25 +126,25 @@
 		<TableHeadCell>Action</TableHeadCell>
 	</TableHead>
 	<TableBody tableBodyClass="divide-y">
-		{#each users as u}
+		{#each Object.entries(users) as [u_id, u]}
 			<TableBodyRow class="text-xs leading-snug">
 				<TableBodyCell>{u.userId}</TableBodyCell>
 				<TableBodyCell>{u.familyName}</TableBodyCell>
 				<TableBodyCell>{u.givenName}</TableBodyCell>
 				<TableBodyCell>{u.email}</TableBodyCell>
-				<TableBodyCell><Toggle checked={u.isActive} on:change={(e) => toggle_active(e, u.userId.toString())} /></TableBodyCell>
+				<TableBodyCell><Toggle checked={toggleChecked[u_id.toString()]} on:click={(e) => toggle_active(e, u_id.toString())} /></TableBodyCell>
 				<TableBodyCell><span class="flex flex-row"><EditOutline class="w-5 h-5" /><TrashBinOutline class="w-5 h-5" /></span></TableBodyCell>
 			</TableBodyRow>
 		{/each}
 	</TableBody>
 </Table>
 
-<Modal title="" bind:open={activeToggleModel} autoclose size="sm" class="w-full">
+<Modal title="" bind:open={toggleConformModal} autoclose size="sm" class="w-full">
 	<svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" /></svg>
-	<p class="mb-4 text-gray-500 dark:text-gray-300 text-center">Are you sure you want to delete this item?</p>
+	<p class="mb-4 text-gray-500 dark:text-gray-300 text-center">Are you sure you want to {toggleChecked[userid.toString()] ? 'disable' : 'enable'} user "{userid.toString()}"?</p>
 	<div class="flex justify-center items-center space-x-4">
 		<Button color="light" on:click={handleCancel}>No, cancel</Button>
-		<Button color="red" on:click={handleDelete}>Yes, I'm sure</Button>
+		<Button color="red" on:click={handleYes}>Yes, I'm sure</Button>
 	</div>
 </Modal>
 
