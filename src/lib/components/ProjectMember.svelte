6d210d0da7a4f75a0c5480236d628bfa64291a83
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2025-02-10 00:54:23 +0100

    More UI elements

--- src/lib/components/ProjectMember.svelte
+++ src/lib/components/ProjectMember.svelte
@@ -18,7 +18,7 @@
 		Checkbox,
 		Dropdown,
 		Label,
-		Radio,
+		Radio, Select,
 		Table,
 		TableBody, TableBodyCell, TableBodyRow,
 		Tooltip
@@ -31,7 +31,8 @@
 		ObjectsColumnOutline,
 		ShareNodesOutline,
 		PlusOutline,
-		StarOutline
+		StarOutline,
+		TrashBinOutline
 	} from 'flowbite-svelte-icons';
 	import { apiClient } from '$lib/shared/apiClient';
 	import type { AuthInfo } from '$lib/interfaces/authinfo';
@@ -43,11 +44,12 @@
 	import type { OldapProject } from '$lib/oldap/classes/project';
 	import { process_api_error } from '$lib/helpers/process_error';
 
-
 	const authinfo_json = sessionStorage.getItem('authinfo')
 	let authinfo: AuthInfo;
 	let projectShortNames: string[] = $state([]);
 	let current_user_permissions: AdminPermission[] = $state([]);
+	let projects_available: {value: string, name: string}[] = $state([])
+	let project_selected: string = $state('')
 
 	let perm_users_disabled = $derived(!(isRoot || current_user_permissions.includes(AdminPermission.ADMIN_USERS)));
 	let perm_lists_disabled = $derived(!(isRoot || current_user_permissions.includes(AdminPermission.ADMIN_LISTS)));
@@ -55,6 +57,15 @@
 	let perm_permissions_disabled = $derived(!(isRoot || current_user_permissions.includes(AdminPermission.ADMIN_PERMISSION_SETS)));
 	let perm_model_disabled = $derived(!(isRoot || current_user_permissions.includes(AdminPermission.ADMIN_MODEL)));
 	let perm_create_disabled = $derived(!(isRoot || current_user_permissions.includes(AdminPermission.ADMIN_CREATE)));
+
+	let perm_users_checked: Record<string, boolean> = $state({});
+	let perm_lists_checked: Record<string, boolean> = $state({});
+	let perm_resources_checked: Record<string, boolean> = $state({});
+	let perm_permissions_checked: Record<string, boolean> = $state({});
+	let perm_model_checked: Record<string, boolean> = $state({});
+	let perm_create_checked: Record<string, boolean> = $state({});
+	let perm_oldap_checked: Record<string, boolean> = $state({});
+
 
 	/**
 	 * Check if the given user has the given AdminPermission
@@ -66,14 +77,6 @@
 	const getPermissions = (user: OldapUser, project_iri: string, permission: AdminPermission) => {
 		return user?.inProject?.find((entry: InProject) => (entry?.project?.toString() === project_iri))?.permissions.includes(permission)
 	}
-
-	let perm_users_checked: Record<string, boolean> = $state({});
-	let perm_lists_checked: Record<string, boolean> = $state({});
-	let perm_resources_checked: Record<string, boolean> = $state({});
-	let perm_permissions_checked: Record<string, boolean> = $state({});
-	let perm_model_checked: Record<string, boolean> = $state({});
-	let perm_create_checked: Record<string, boolean> = $state({});
-	let perm_oldap_checked: Record<string, boolean> = $state({});
 
 	/**
 	 * Get the permissions with the given project shortname (exported)
@@ -99,6 +102,18 @@
 		return projectShortNames;
 	};
 
+	/**
+	 * Adds the selected project to the list of projects the user is member of
+	 */
+	const add_project = () => {
+		if (project_selected !== "") {
+			projectShortNames.push(project_selected)
+			projects_available = projects_available.filter(item => item.value !== project_selected)
+		}
+		console.log("****>", project_selected);
+		project_selected = "";
+	}
+
 	current_user.inProject.forEach((item: InProject) => {
 		if (item.project.toString() === current_project.projectIri.toString()) {
 			current_user_permissions = item.permissions;
@@ -108,6 +123,11 @@
 	if (authinfo_json) {
 		authinfo = JSON.parse(authinfo_json);
 		if (isRoot) {
+			//
+			// the current_user has root privilegs (ADMIN_OLDAP)
+			//
+			// let's get all the projects existing
+			//
 			const all_projects_config = {
 				headers: {
 					'Accept': 'application/json',
@@ -137,102 +157,63 @@
 								perm_oldap_checked[data[1]] = getPermissions(user, data[0], AdminPermission.ADMIN_OLDAP) ?? false;
 							}
 							else { // The user is not (yet) associated with the project...
-
+									projects_available.push({value: data[1], name: data[1]});
+								}
 							}
-						}
+						});
+					})
+					.catch((err) => {
+						process_api_error(err)
 					});
-				})
-				.catch((err) => {
-					process_api_error(err)
-				});
+			}
+			else {
+				projectShortNames.push(current_project.projectShortName);
+
+				perm_users_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_USERS) ?? false;
+				perm_lists_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_LISTS) ?? false;
+				perm_resources_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_RESOURCES) ?? false;
+				perm_permissions_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_PERMISSION_SETS) ?? false;
+				perm_model_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_MODEL) ?? false;
+				perm_create_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_CREATE) ?? false;
+				perm_oldap_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_OLDAP) ?? false;
+
+			}
 		}
 		else {
-			projectShortNames.push(current_project.projectShortName);
-
-			perm_users_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_USERS) ?? false;
-			perm_lists_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_LISTS) ?? false;
-			perm_resources_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_RESOURCES) ?? false;
-			perm_permissions_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_PERMISSION_SETS) ?? false;
-			perm_model_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_MODEL) ?? false;
-			perm_create_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_CREATE) ?? false;
-			perm_oldap_checked[current_project.projectShortName.toString()] = getPermissions(user, current_project.projectIri.toString(), AdminPermission.ADMIN_OLDAP) ?? false;
-
+			errorInfoStore.set({
+				errormsg: m.userdata_corrupted(),
+				errorcode: 500,
+				severity: Severity.ERROR,
+			});
 		}
-	}
-	else {
-		errorInfoStore.set({
-			errormsg: m.userdata_corrupted(),
-			errorcode: 500,
-			severity: Severity.ERROR,
-		});
-	}
 
 </script>
-<Table class="w-full">
-	<TableBody>
-	{#each projectShortNames as shortname}
-		<TableBodyRow>
-			<TableBodyCell class="whitespace-nowrap text-right pr-4"><Label class="text-right">{shortname}: </Label></TableBodyCell>
-			<TableBodyCell class="flex flex-wrap gap-4 items-center">
-				<Checkbox disabled={perm_users_disabled} bind:checked={perm_users_checked[shortname]}><UserOutline /><Tooltip>{m.perm_user()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_lists_disabled} bind:checked={perm_lists_checked[shortname]}><ListOutline /><Tooltip>{m.perm_lists()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_resources_disabled} bind:checked={perm_resources_checked[shortname]}><ShieldCheckOutline /><Tooltip>{m.perm_resources()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_permissions_disabled} bind:checked={perm_permissions_checked[shortname]}><ObjectsColumnOutline /><Tooltip>{m.perm_permissions()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_model_disabled} bind:checked={perm_model_checked[shortname]}><ShareNodesOutline /><Tooltip>{m.perm_model()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_create_disabled} bind:checked={perm_create_checked[shortname]}><PlusOutline /><Tooltip>{m.perm_create()}</Tooltip></Checkbox>
-				<Checkbox disabled={!isRoot} bind:checked={perm_oldap_checked[shortname]}><StarOutline /><Tooltip>{m.perm_system()}</Tooltip></Checkbox>
-			</TableBodyCell>
-		</TableBodyRow>
-	{/each}
-	</TableBody>
-</Table>
-<!--
-<div class="flex flex-col gap-2">
-	{#each projectShortNames as shortname}
-		<div class="grid grid-cols-[max-content_1fr] items-center gap-4">
-			<Label class="text-right">{shortname}: </Label>
-			<div class="flex flex-wrap gap-4 flex-grow">
-				<Checkbox disabled={perm_users_disabled} bind:checked={perm_users_checked[shortname]}><UserOutline /><Tooltip>{m.perm_user()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_lists_disabled} bind:checked={perm_lists_checked[shortname]}><ListOutline /><Tooltip>{m.perm_lists()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_resources_disabled} bind:checked={perm_resources_checked[shortname]}><ShieldCheckOutline /><Tooltip>{m.perm_resources()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_permissions_disabled} bind:checked={perm_permissions_checked[shortname]}><ObjectsColumnOutline /><Tooltip>{m.perm_permissions()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_model_disabled} bind:checked={perm_model_checked[shortname]}><ShareNodesOutline /><Tooltip>{m.perm_model()}</Tooltip></Checkbox>
-				<Checkbox disabled={perm_create_disabled} bind:checked={perm_create_checked[shortname]}><PlusOutline /><Tooltip>{m.perm_create()}</Tooltip></Checkbox>
-				<Checkbox disabled={!isRoot} bind:checked={perm_oldap_checked[shortname]}><StarOutline /><Tooltip>{m.perm_system()}</Tooltip></Checkbox>
-			</div>
-		</div>
-	{/each}
-</div>
-
-<Accordion activeClass="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800" inactiveClass="text-gray-500 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800">
-	{#each projectShortNames as shortname}
-		<AccordionItem paddingDefault="p-1 px-2">
-			<span slot="header">{shortname}</span>
-			<Button outline size="xs"> Admin Permissions <ChevronDownOutline class="w-6 h-6 ms-2 text-red-800 dark:text-red-800" /></Button>
-			<Dropdown class="w-44 p-3 space-y-3 text-sm">
-				<li>
-					<Checkbox disabled={perm_users_disabled} bind:checked={perm_users_checked[shortname]}>{m.perm_user()} {shortname}</Checkbox>
-				</li>
-				<li>
-					<Checkbox disabled={perm_lists_disabled} bind:checked={perm_lists_checked[shortname]}>{m.perm_lists()}</Checkbox>
-				</li>
-				<li>
-					<Checkbox disabled={perm_resources_disabled} bind:checked={perm_resources_checked[shortname]}>{m.perm_resources()}</Checkbox>
-				</li>
-				<li>
-					<Checkbox disabled={perm_permissions_disabled} bind:checked={perm_permissions_checked[shortname]}>{m.perm_permissions()}</Checkbox>
-				</li>
-				<li>
-					<Checkbox disabled={perm_model_disabled} bind:checked={perm_model_checked[shortname]}>{m.perm_model()}</Checkbox>
-				</li>
-				<li>
-					<Checkbox disabled={perm_create_disabled} bind:checked={perm_create_checked[shortname]}>{m.perm_create()}</Checkbox>
-				</li>
-				<li>
-					<Checkbox disabled={!isRoot} bind:checked={perm_oldap_checked[shortname]}>{m.perm_system()}</Checkbox>
-				</li>
-			</Dropdown>
-		</AccordionItem>
+<div class="flex flex-col space-y-6">
+	<div class="inline-flex items-center gap-2">
+		<Select size=sm placeholder="project..." class="w-auto min-w-[max-content] pr-8" items={projects_available} bind:value={project_selected} />
+		<Button size=xs onclick={add_project}>Add</Button>
+	</div>
+	<Table class="w-full">
+		<TableBody>
+		{#each projectShortNames as shortname}
+			<TableBodyRow>
+				<TableBodyCell class="whitespace-nowrap text-right pr-4">
+					<span class="inline-flex items-center gap-2">
+    				<Label class="text-right">{shortname}:</Label>
+						<Button pill={true} class="!p-2" size="xs"><TrashBinOutline class="w-3 h-3" /></Button>
+  				</span>
+				</TableBodyCell>
+				<TableBodyCell class="flex flex-wrap gap-4 items-center">
+					<Checkbox disabled={perm_users_disabled} bind:checked={perm_users_checked[shortname]}><UserOutline /><Tooltip>{m.perm_user()}</Tooltip></Checkbox>
+					<Checkbox disabled={perm_lists_disabled} bind:checked={perm_lists_checked[shortname]}><ListOutline /><Tooltip>{m.perm_lists()}</Tooltip></Checkbox>
+					<Checkbox disabled={perm_resources_disabled} bind:checked={perm_resources_checked[shortname]}><ShieldCheckOutline /><Tooltip>{m.perm_resources()}</Tooltip></Checkbox>
+					<Checkbox disabled={perm_permissions_disabled} bind:checked={perm_permissions_checked[shortname]}><ObjectsColumnOutline /><Tooltip>{m.perm_permissions()}</Tooltip></Checkbox>
+					<Checkbox disabled={perm_model_disabled} bind:checked={perm_model_checked[shortname]}><ShareNodesOutline /><Tooltip>{m.perm_model()}</Tooltip></Checkbox>
+					<Checkbox disabled={perm_create_disabled} bind:checked={perm_create_checked[shortname]}><PlusOutline /><Tooltip>{m.perm_create()}</Tooltip></Checkbox>
+					<Checkbox disabled={!isRoot} bind:checked={perm_oldap_checked[shortname]}><StarOutline /><Tooltip>{m.perm_system()}</Tooltip></Checkbox>
+				</TableBodyCell>
+			</TableBodyRow>
 		{/each}
-</Accordion>
--->
+		</TableBody>
+	</Table>
+</div>
